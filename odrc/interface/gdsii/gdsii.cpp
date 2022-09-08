19eaee152ca844ce379c5291c0f3b4b83a1b5f16
Zhuolun Leon HE <zleonhe@gmail.com>
2022-09-08 19:31:47 +0800

    style(*): align consecutive assignments and declarations

--- odrc/interface/gdsii/gdsii.cpp
+++ odrc/interface/gdsii/gdsii.cpp
@@ -1,5 +1,6 @@
 #include <odrc/interface/gdsii/gdsii.hpp>
 
+#include <cassert>
 #include <cstddef>
 #include <cstring>
 #include <exception>
@@ -22,7 +23,7 @@ double parse_real64(const std::byte* bytes) {
     data = (data << 8) + std::to_integer<uint64_t>(bytes[i]);
   }
 
-  int exponent = ((data & 0x7f00'0000'0000'0000) >> 56) - 64;
+  int    exponent = ((data & 0x7f00'0000'0000'0000) >> 56) - 64;
   double mantissa =
       (data & 0x00ff'ffff'ffff'ffff) / static_cast<double>(1ll << 56);
   double result = mantissa * (1 << (exponent * 4));  // m*16^e
@@ -31,7 +32,7 @@ double parse_real64(const std::byte* bytes) {
 
 void library::read(const std::filesystem::path& file_path) {
   std::vector<std::byte> buffer(65536);
-  std::ifstream ifs(file_path, std::ios::in | std::ios::binary);
+  std::ifstream          ifs(file_path, std::ios::in | std::ios::binary);
   if (not ifs) {
     throw std::runtime_error("Cannot open " + file_path.string() + ": " +
                              std::strerror(errno));
@@ -39,9 +40,9 @@ void library::read(const std::filesystem::path& file_path) {
   while (true) {
     // read record header
     ifs.read(reinterpret_cast<char*>(buffer.data()), 4);
-    int record_length = parse_int16(&buffer[0]);
-    record_type rtype = static_cast<record_type>(buffer[2]);
-    data_type dtype = static_cast<data_type>(buffer[3]);
+    int         record_length = parse_int16(&buffer[0]);
+    record_type rtype         = static_cast<record_type>(buffer[2]);
+    data_type   dtype         = static_cast<data_type>(buffer[3]);
     ifs.read(reinterpret_cast<char*>(buffer.data() + 4), record_length - 4);
 
     switch (rtype) {
@@ -51,16 +52,16 @@ void library::read(const std::filesystem::path& file_path) {
         break;
       case record_type::BGNLIB:
         assert(dtype == data_type::int16);
-        mtime.year = parse_int16(&buffer[4]);
-        mtime.month = parse_int16(&buffer[6]);
-        mtime.day = parse_int16(&buffer[8]);
-        mtime.hour = parse_int16(&buffer[10]);
+        mtime.year   = parse_int16(&buffer[4]);
+        mtime.month  = parse_int16(&buffer[6]);
+        mtime.day    = parse_int16(&buffer[8]);
+        mtime.hour   = parse_int16(&buffer[10]);
         mtime.minute = parse_int16(&buffer[12]);
         mtime.second = parse_int16(&buffer[14]);
-        atime.year = parse_int16(&buffer[16]);
-        atime.month = parse_int16(&buffer[18]);
-        atime.day = parse_int16(&buffer[20]);
-        atime.hour = parse_int16(&buffer[22]);
+        atime.year   = parse_int16(&buffer[16]);
+        atime.month  = parse_int16(&buffer[18]);
+        atime.day    = parse_int16(&buffer[20]);
+        atime.hour   = parse_int16(&buffer[22]);
         atime.minute = parse_int16(&buffer[24]);
         atime.second = parse_int16(&buffer[26]);
         break;
@@ -74,7 +75,7 @@ void library::read(const std::filesystem::path& file_path) {
       case record_type::UNITS:
         assert(dtype == data_type::real64);
         dbu_in_user_unit = parse_real64(&buffer[4]);
-        dbu_in_meter = parse_real64(&buffer[12]);
+        dbu_in_meter     = parse_real64(&buffer[12]);
         break;
       case record_type::ENDLIB:
         assert(dtype == data_type::no_data);
--- odrc/interface/gdsii/gdsii.hpp
+++ odrc/interface/gdsii/gdsii.hpp
@@ -7,42 +7,42 @@
 
 namespace odrc::gdsii {
 enum class record_type : std::underlying_type_t<std::byte> {
-  HEADER = 0x00,
-  BGNLIB = 0x01,
+  HEADER  = 0x00,
+  BGNLIB  = 0x01,
   LIBNAME = 0x02,
-  UNITS = 0x03,
-  ENDLIB = 0x04,
+  UNITS   = 0x03,
+  ENDLIB  = 0x04,
 };
 enum class data_type : std::underlying_type_t<std::byte> {
-  no_data = 0x00,
-  int16 = 0x02,
-  int32 = 0x03,
-  real32 = 0x04,
-  real64 = 0x05,
+  no_data      = 0x00,
+  int16        = 0x02,
+  int32        = 0x03,
+  real32       = 0x04,
+  real64       = 0x05,
   ascii_string = 0x06,
 };
 
 // Data parsers
 int16_t parse_int16(const std::byte* bytes);
-double parse_real64(const std::byte* bytes);
+double  parse_real64(const std::byte* bytes);
 
 class library {
  public:
   struct datetime {
-    int year = -1;
-    int month = -1;
-    int day = -1;
-    int hour = -1;
+    int year   = -1;
+    int month  = -1;
+    int day    = -1;
+    int hour   = -1;
     int minute = -1;
     int second = -1;
   };
   void read(const std::filesystem::path& file_path);
   // meta info
-  int version = -1;
-  datetime mtime;
-  datetime atime;
+  int         version = -1;
+  datetime    mtime;
+  datetime    atime;
   std::string name;
-  double dbu_in_user_unit;
-  double dbu_in_meter;
+  double      dbu_in_user_unit;
+  double      dbu_in_meter;
 };
 }  // namespace odrc::gdsii
