738ee9422ba4394de9b1b0b07de793d4bfe95d93
Zhuolun Leon HE <zleonhe@gmail.com>
2022-09-30 16:45:07 +0800

    feat(sweepline): add parallel prefix construction

--- odrc/infrastructure/sweepline/sweepline.hpp
+++ odrc/infrastructure/sweepline/sweepline.hpp
@@ -1,11 +1,41 @@
 #pragma once
 
+#ifdef __CUDACC__
+#include <cuda_runtime.h>
+#endif
+
+#include <cassert>
 #include <stdexcept>
 #include <type_traits>
 
 #include <odrc/infrastructure/execution.hpp>
 
 namespace odrc::core {
+
+#ifdef __CUDACC__
+template <typename EventPointIt, typename Status, typename Fold>
+__global__ void _build_prefix(EventPointIt first,
+                              EventPointIt last,
+                              Status*      status,
+                              Fold         fold) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (first + i < last) {
+    status[i] = fold(first, first + i + 1);
+  }
+}
+
+template <typename EventPointIt, typename Status, typename Fold>
+void _sweepline_par(EventPointIt first,
+                    EventPointIt last,
+                    Status*      status,
+                    Fold         fold) {
+  int n = std::distance(first, last);
+  _build_prefix<<<(n + 127) / 128, 128>>>(first, last, status, fold);
+  cudaDeviceSynchronize();
+  cudaError_t cuda_status = cudaGetLastError();
+  assert(cuda_status == cudaSuccess);
+}
+#endif
 
 template <typename EventPointIt, typename Status, typename Op>
 void _sweepline(EventPointIt first, EventPointIt last, Status* status, Op op) {
@@ -28,7 +58,11 @@ void sweepline(ExecutionPolicy&& policy,
                     odrc::execution::sequenced_policy>) {
     _sweepline(first, last, status, op);
   } else {
+#ifdef __CUDACC__
+    _sweepline_par(first, last, status, op);
+#else
     throw std::logic_error("Parallel sweepline is not implemented.");
+#endif
   }
 }
 
