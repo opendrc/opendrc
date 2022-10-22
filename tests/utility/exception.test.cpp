57c451ebc545d083098c0345687ffc41f4941e3f
Zhuolun Leon HE <zleonhe@gmail.com>
2022-10-22 12:48:03 +0800

    feat(exception): add open_file_error and fix all interfaces

--- tests/utility/exception.test.cpp
+++ tests/utility/exception.test.cpp
@@ -4,11 +4,25 @@
 
 TEST_SUITE("[OpenDRC] odrc::exception tests") {
   TEST_CASE("test throw not_implemented_error") {
-    CHECK_THROWS_AS(throw odrc::not_implemented_error("test"),
-                    odrc::not_implemented_error);
+    CHECK_THROWS_WITH_AS(throw odrc::not_implemented_error(),
+                         "Function not implemented.",
+                         odrc::not_implemented_error);
+    CHECK_THROWS_WITH_AS(
+        throw odrc::not_implemented_error("test not implemented"),
+        "test not implemented", odrc::not_implemented_error);
   }
 
-  TEST_CASE("test INVAILD_FILE_ERROR") {
-    CHECK_THROWS_AS(throw odrc::invalid_file("test"), odrc::invalid_file);
+  TEST_CASE("test throw open_file_error") {
+    CHECK_THROWS_WITH_AS(throw odrc::open_file_error(), "Open file error.",
+                         odrc::open_file_error);
+    CHECK_THROWS_WITH_AS(throw odrc::open_file_error("test open file error"),
+                         "test open file error", odrc::open_file_error);
+  }
+
+  TEST_CASE("test throw invalid_file") {
+    CHECK_THROWS_WITH_AS(throw odrc::invalid_file(), "Invalid file.",
+                         odrc::invalid_file);
+    CHECK_THROWS_WITH_AS(throw odrc::invalid_file("test invalid file"),
+                         "test invalid file", odrc::invalid_file);
   }
 }
