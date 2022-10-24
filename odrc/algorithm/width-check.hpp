db15051f6e1aeb77863dec41581ea0f509327064
Zhuolun Leon HE <zleonhe@gmail.com>
2022-10-24 17:29:07 +0800

    feat(width-check): reimplement width-check with hierarchy

--- odrc/algorithm/width-check.hpp
+++ odrc/algorithm/width-check.hpp
@@ -3,5 +3,5 @@
 #include <odrc/core/database.hpp>
 
 namespace odrc {
-    void width_check(const odrc::core::database &db, int layer, int threshold);
+void width_check(const odrc::core::database& db, int layer, int threshold);
 }
