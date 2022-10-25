d35573faa198f6aaf43f233de0e93480666579ec
zyh0911 <1815451667@qq.com>
2022-10-25 19:45:58 +0800

    eliminates branch conflicts

--- odrc/core/database.hpp
+++ odrc/core/database.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-<<<<<<< HEAD
 #include <algorithm>
 #include <iostream>
 #include <map>
@@ -9,14 +8,6 @@
 #include <string>
 #include <vector>
 
-=======
-#include <string>
-#include <vector>
-
-#include <odrc/core/cell.hpp>
-#include <odrc/utility/datetime.hpp>
-
->>>>>>> 39871cac67c41f4a4d8467f7735ff4cdefdb6cc0
 namespace odrc::core {
 
 class database {
@@ -29,20 +20,14 @@ class database {
   double               dbu_in_user_unit;
   double               dbu_in_meter;
 
-<<<<<<< HEAD
   std::map<std::string, int64_t>          cell_layer;
   std::map<std::string, std::vector<int>> min_bound_rect;
-=======
->>>>>>> 39871cac67c41f4a4d8467f7735ff4cdefdb6cc0
   // layout
   cell& create_cell() { return cells.emplace_back(); }
 
   std::vector<cell> cells;
-<<<<<<< HEAD
 
   void min_bound();
   void layer_sort();
-=======
->>>>>>> 39871cac67c41f4a4d8467f7735ff4cdefdb6cc0
 };
 }  // namespace odrc::core
