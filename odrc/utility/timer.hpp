e8aef662e7073d3065766b0a4f9b8221eda77e00
CNILeo <chn.leo.zheng@gmail.com>
2022-10-28 16:49:07 +0800

    feat(timer): impl reset interface

--- odrc/utility/timer.hpp
+++ odrc/utility/timer.hpp
@@ -11,23 +11,32 @@ namespace odrc::util {
 class timer {
  public:
   timer(std::string tag, logger* time_logger) {
-    _tag              = tag;
-    _logger           = time_logger;
+    _tag    = tag;
+    _logger = time_logger;
+  }
+
+  ~timer() { _logger->info("Timer", "[{}] {} ms", _tag, elapsed_ms); }
+
+  void start() {
     _begin_time_point = std::chrono::high_resolution_clock::now();
   }
 
-  ~timer() {
+  void pause() {
     const auto _end_time_point = std::chrono::high_resolution_clock::now();
-    int64_t elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                             _end_time_point - _begin_time_point)
-                             .count();
+    elapsed_ms += std::chrono::duration_cast<std::chrono::milliseconds>(
+                      _end_time_point - _begin_time_point)
+                      .count();
+  }
 
-    _logger->info("Timer", "[{}] {} ms", _tag, elapsed_ms);
+  void reset(std::string message = "") {
+    _logger->info("Timer", "[{}] {} {} ms", _tag, message, elapsed_ms);
+    elapsed_ms = 0;
   }
 
  private:
   std::string                                                 _tag;
   logger*                                                     _logger;
+  int64_t                                                     elapsed_ms = 0;
   std::chrono::time_point<std::chrono::high_resolution_clock> _begin_time_point;
 };
 
--- tests/utility/timer.test.cpp
+++ tests/utility/timer.test.cpp
@@ -7,31 +7,47 @@
 #include <thread>
 
 void check_elapsed_time(const std::string& filename,
-                        const int64_t      elapsed_time) {
+                        const int          sleep_count,
+                        const int          reset_count,
+                        const int64_t      sleep_time) {
   std::ifstream ifs(filename);
   if (!ifs) {
     throw std::runtime_error("Failed open file ");
   }
 
   std::string log_output;
-  std::getline(ifs, log_output);
+  for (int i = 0; i < reset_count; ++i) {
+    std::getline(ifs, log_output);
+    CHECK(log_output.find(std::to_string(sleep_time) + " ms") !=
+          std::string::npos);
+  }
 
-  CHECK(log_output.find(std::to_string(elapsed_time) + " ms") !=
+  std::getline(ifs, log_output);
+  const int64_t expect_time = (sleep_count - reset_count) * sleep_time;
+  CHECK(log_output.find(std::to_string(expect_time) + " ms") !=
         std::string::npos);
 }
 
 TEST_SUITE("[OpenDRC] odrc::timer tests") {
   TEST_CASE("test timer") {
     int64_t            sleep_time   = 2000;
+    int                sleep_count  = 10;
+    int                reset_count  = 5;
     const std::string  log_filename = "log_timer_test.txt";
     odrc::util::logger logger(log_filename, odrc::util::log_level::trace,
                               false);
     {
       odrc::util::timer test_timer("test", &logger);
-      std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+      for (int i = 0; i < sleep_count; ++i) {
+        test_timer.start();
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+        test_timer.pause();
+        if (i < reset_count)
+          test_timer.reset("reset " + std::to_string(i));
+      }
     }
     logger.flush();
-    check_elapsed_time(log_filename, sleep_time);
+    check_elapsed_time(log_filename, sleep_count, reset_count, sleep_time);
     std::remove(log_filename.c_str());
   }
 }
