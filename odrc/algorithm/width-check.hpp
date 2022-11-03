928b6403cf2792e74398ab02885bd67f5828c16b
Zhuolun Leon HE <zleonhe@gmail.com>
2022-11-03 19:30:06 +0800

    [DAC23] cpu version of hierarchical width-check

--- odrc/algorithm/width-check.hpp
+++ odrc/algorithm/width-check.hpp
@@ -4,4 +4,5 @@
 
 namespace odrc {
 void width_check(const odrc::core::database& db, int layer, int threshold);
+void width_check_cpu(const odrc::core::database& db, int layer, int threshold);
 }
--- odrc/main.cpp
+++ odrc/main.cpp
@@ -4,6 +4,8 @@
 #include <odrc/algorithm/width-check.hpp>
 #include <odrc/gdsii/gdsii.hpp>
 
+#include <odrc/utility/timer.hpp>
+
 void help() {
   std::cerr << "Usage: ./odrc <gds_in>" << std::endl;
 }
@@ -15,9 +17,27 @@ int main(int argc, char* argv[]) {
   }
   try {
     auto db = odrc::gdsii::read(argv[1]);
-    db.update_depth_and_mbr();
-    odrc::width_check(db, 11, 650);
-    odrc::space_check(db, 11, 11, 650);
+    // db.update_depth_and_mbr();
+    // for(auto &c: db.cells) {
+    //   std::cout << c.name << ": " << c.depth << std::endl;
+    // }
+    // return 0;
+    {
+      odrc::util::logger logger("/dev/null", odrc::util::log_level::info, true);
+      odrc::util::timer  t("t", &logger);
+      t.start();
+      odrc::width_check_cpu(db, 20, 18);
+      t.pause();
+    }
+    {
+      odrc::util::logger logger("/dev/null", odrc::util::log_level::info, true);
+      odrc::util::timer  t("t", &logger);
+      t.start();
+      odrc::width_check_cpu(db, 30, 18);
+      t.pause();
+    }
+    // odrc::width_check(db, 11, 650);
+    // odrc::space_check(db, 11, 11, 650);
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
     return 1;
