0e6b7d6a2cf400278e8c3d9076608cafa3b0777e
zyh0911 <1815451667@qq.com>
2022-11-06 12:33:30 +0800

    refactor(interval tree): refactor the code of fucntions

--- examples/overlap_query.cpp
+++ examples/overlap_query.cpp
@@ -1,78 +1,64 @@
 #include <vector>
 
+#include <iostream>
 #include <odrc/core/interval_tree.hpp>
 #include <odrc/gdsii/gdsii.hpp>
-
-
-void get_polygon_edge(const odrc::core::cell*            cells,
-                      std::vector<odrc::core::interval>* sorted_edge,
-                      int                                layer,
-                      int                                ref_x,
-                      int                                ref_y,//the name of get_xx function
-                      int*                               id) {//id =sorted_edge.size()/2
-  for (const auto& poly : cells->polygons) {
-    if (poly.layer == layer) {
-      sorted_edge->emplace_back(odrc::core::interval{
-          poly.mbr[0] + ref_x, poly.mbr[1] + ref_x, poly.mbr[2] + ref_y, *id});
-      sorted_edge->emplace_back(odrc::core::interval{
-          poly.mbr[0] + ref_x, poly.mbr[1] + ref_x, poly.mbr[3] + ref_y, -*id});
-      id++;
-    }
-  }
-}
-
-void add_edge(odrc::core::database&              db,
-              std::vector<odrc::core::interval>* sorted_edge,
-              const odrc::core::cell*            cells,
-              int                                layer,
-              int*                               id) {
-  get_polygon_edge(cells, sorted_edge, layer, 0, 0, id);
-  if (cells->depth > 1) {
-    for (const auto& cell_ref : cells->cell_refs) {
-      auto the_cell = db.get_cell(cell_ref.cell_name);
-      if (the_cell.layers == layer) {//
-        int ref_x = cell_ref.ref_point.x;
-        int ref_y = cell_ref.ref_point.y;
-        get_polygon_edge(&the_cell, sorted_edge, layer, ref_x, ref_y, id);
-      }
-      //
-    }
-  }
-}
-
 std::vector<std::pair<int, int>> overlap_query(odrc::core::database& db,
                                                const int&            layer) {
   std::vector<odrc::core::interval> sorted_edge;
   std::vector<std::pair<int, int>>  overlap_cells;
-  int                               id = 1;
 
   // input data
   for (const auto& cell : db.cells) {
-    add_edge(db, &sorted_edge, &cell, layer, &id);
+    if (cell.is_touching(layer)) {
+      for (const auto& poly : cell.polygons) {
+        if (layer == poly.layer) {
+          sorted_edge.emplace_back(
+              odrc::core::interval{poly.mbr[0], poly.mbr[1], poly.mbr[2],
+                                   sorted_edge.size() / 2 + 1});
+          sorted_edge.emplace_back(
+              odrc::core::interval{poly.mbr[0], poly.mbr[1], poly.mbr[3],
+                                   -sorted_edge.size() / 2 - 1});
+        }
+      }
+      for (const auto& cell_ref :
+           cell.cell_refs) {  // only support for the max depth of cells is 2
+        auto the_cell = db.get_cell(cell_ref.cell_name);
+        if (the_cell.is_touching(layer)) {
+          int ref_x = cell_ref.ref_point.x;
+          int ref_y = cell_ref.ref_point.y;
+          for (const auto& poly : the_cell.polygons) {
+            if (layer == poly.layer) {
+              sorted_edge.emplace_back(odrc::core::interval{
+                  poly.mbr[0] + ref_x, poly.mbr[1] + ref_x, poly.mbr[2] + ref_y,
+                  sorted_edge.size() / 2 + 1});
+              sorted_edge.emplace_back(odrc::core::interval{
+                  poly.mbr[0] + ref_x, poly.mbr[1] + ref_x, poly.mbr[3] + ref_y,
+                  -sorted_edge.size() / 2 - 1});
+            }
+          }
+        }
+      }
+    }
   }
   std::sort(sorted_edge.begin(), sorted_edge.end(),
             [](const odrc::core::interval& a, const odrc::core::interval& b) {
               return a.y < b.y;
             });
-
   odrc::core::interval_tree tree;
   for (int edge = 0; edge != sorted_edge.size(); edge++) {
     if (sorted_edge[edge].id > 0) {
-      if (edge == 0) {
-        tree.add_node(&sorted_edge[edge]);
-      } else {// user 
-        tree.get_overlapping_intervals(&sorted_edge[edge], 0, &overlap_cells);
-        tree.add_interval(&sorted_edge[edge], 0);
-      }
+      auto overlap_intervals =
+          tree.get_intervals_overlapping_with(sorted_edge[edge]);
+      overlap_cells.insert(overlap_cells.end(), overlap_intervals.begin(),
+                           overlap_intervals.end());
+      tree.add_interval(sorted_edge[edge]);
     } else {
-      tree.delete_interval(&sorted_edge[edge], 0);
+      tree.delete_interval(sorted_edge[edge]);
     }
   }
   return overlap_cells;
 }
-
-
-
 int main(int argc, char* argv[]) {
   auto db = odrc::gdsii::read(argv[1]);
   db.update_depth_and_mbr();
--- odrc/core/interval_tree.hpp
+++ odrc/core/interval_tree.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <algorithm>
+#include <iostream>
 #include <vector>
-
 namespace odrc::core {
+
+using interval_pairs = std::vector<std::pair<int, int>>;
 
 struct interval {
   int x_left;
@@ -18,57 +20,60 @@ struct node {
   int                   right_child = -1;
   std::vector<interval> lines_start;
   std::vector<interval> lines_end;
-  bool   has_left_child(){return ;}//
+
+  bool has_left_child() { return left_child != -1; }
+  bool has_right_child() { return right_child != -1; }
 };
 
 class interval_tree {
  public:
-  int  add_node(interval* edge);  // add a node into the tree
-  void add_interval(interval* edge,
-                    const int node);  // add an interval into the interval tree
+  void add_interval(
+      interval& edge,
+      const int node = 0);  // add an interval into the interval tree
   void delete_interval(
-      interval* edge,
-      const int node);  // delete an interval from the interval tree
-  void get_overlapping_intervals(
-      interval* edge,
-      const int node,
-      std::vector<std::pair<int, int>>*
-          overlap_interval);  // get the overlapping intervals
+      interval& edge,
+      const int node = 0);  // delete an interval from the interval tree
+  interval_pairs get_intervals_overlapping_with(
+      interval& edge,
+      const int node = 0);  // get the intervals overlapping with the given edge
 
  private:
   std::vector<node> nodes;
-  void _add_interval_to_node(interval* edge,
-                   const int node);  // add a segment into an existing node
-  void _get_intervals_containing_point(
+
+  int  _add_node(interval& edge);
+  void _add_interval_to_node(
+      interval& edge,
+      const int node);  // add a segment into an existing node
+  interval_pairs _get_intervals_containing_point(
       const int point,
       const int edge_id,
-      const int node,
-      std::vector<std::pair<int, int>>*
-          overlap_interval);  // get intervals containing the point
+      const int node);  // get intervals containing the point
 };
 
-inline int interval_tree::add_node(interval* edge) {
+inline int interval_tree::_add_node(interval& edge) {
   nodes.emplace_back();
-  nodes.back().x_mid = (edge->x_left + edge->x_right) / 2;
-  nodes.back().lines_start.emplace_back(*edge);
-  nodes.back().lines_end.emplace_back(*edge);
+  nodes.back().x_mid = (edge.x_left + edge.x_right) / 2;
+  nodes.back().lines_start.emplace_back(edge);
+  nodes.back().lines_end.emplace_back(edge);
   return nodes.size() - 1;
 }
 
-inline void interval_tree::get_intervals_containing_point(
-    const int                         point,
-    const int                         edge_id,
-    const int                         node,
-    std::vector<std::pair<int, int>>* overlap_intervals) {// name of function
-  if (point == nodes.at(node).x_mid) {
-    for (const auto& edge : nodes.at(node).lines_start) {
-      overlap_intervals->emplace_back(std::make_pair(edge_id, edge.id));
+inline interval_pairs interval_tree::_get_intervals_containing_point(
+    const int point,
+    const int id,
+    const int node) {
+  interval_pairs overlap_intervals;
+  if (point ==
+      nodes.at(node).x_mid) {  // if the point is the middle of the node ,then
+                               // all intervals will contain this point
+    for (const auto& edge : nodes.at(node).lines_end) {
+      overlap_intervals.emplace_back(std::make_pair(id, edge.id));
     }
   } else if (point < nodes.at(node).x_mid) {
     for (auto edge = nodes.at(node).lines_start.begin();
          edge != nodes.at(node).lines_start.end(); edge++) {
       if (edge->x_left <= point) {
-        overlap_intervals->emplace_back(std::make_pair(edge_id, edge->id));
+        overlap_intervals.emplace_back(std::make_pair(id, edge->id));
       } else {
         break;
       }
@@ -77,90 +82,113 @@ inline void interval_tree::get_intervals_containing_point(
     for (auto edge = nodes.at(node).lines_end.rbegin();
          edge != nodes.at(node).lines_end.rend(); edge++) {
       if (edge->x_right >= point) {
-        overlap_intervals->emplace_back(std::make_pair(edge_id, edge->id));
+        overlap_intervals.emplace_back(std::make_pair(id, edge->id));
       } else {
         break;
       }
     }
   }
+  return overlap_intervals;
 }
 
-inline void interval_tree::get_overlapping_intervals(
-    interval*                         edge,
-    int                               node,
-    std::vector<std::pair<int, int>>* overlap_interval) {//
-  int left_child_offset  = nodes.at(node).left_child;
-  int right_child_offset = nodes.at(node).right_child;
-  if (edge->x_right <= nodes.at(node).x_mid) {
-    get_intervals_containing_point(edge->x_right, edge->id, node,
-                                   overlap_interval);
-    if (is_exist(left_child_offset)) {
-      get_overlapping_intervals(edge, left_child_offset, overlap_interval);
-    }
-
-  } else if (edge->x_left >= nodes.at(node).x_mid) {
-    get_intervals_containing_point(edge->x_left, edge->id, node,
-                                   overlap_interval);
-    if (is_exist(right_child_offset)) {
-      get_overlapping_intervals(edge, right_child_offset, overlap_interval);
-    }
+inline interval_pairs interval_tree::get_intervals_overlapping_with(
+    interval& edge,
+    int       node) {
+  interval_pairs overlap_intervals;
+  if (nodes.empty()) {  // if node is empty, return null
+    return overlap_intervals;
   } else {
-    get_intervals_containing_point(nodes.at(node).x_mid, edge->id, node,
-                                   overlap_interval);
-    if (is_exist(left_child_offset)) {
-      get_overlapping_intervals(edge, left_child_offset, overlap_interval);
+    int left_child_offset  = nodes.at(node).left_child;
+    int right_child_offset = nodes.at(node).right_child;
+    if (edge.x_right <= nodes.at(node).x_mid) {
+      overlap_intervals =
+          _get_intervals_containing_point(edge.x_right, edge.id, node);
+      if (nodes.at(node).has_left_child()) {  // if node has a left child, jump
+                                              // to left child node
+        auto intervals =
+            get_intervals_overlapping_with(edge, left_child_offset);
+        overlap_intervals.insert(overlap_intervals.end(), intervals.begin(),
+                                 intervals.end());
+      }
+    } else if (edge.x_left >= nodes.at(node).x_mid) {
+      overlap_intervals =
+          _get_intervals_containing_point(edge.x_left, edge.id, node);
+      if (nodes.at(node).has_right_child()) {
+        auto intervals =
+            get_intervals_overlapping_with(edge, right_child_offset);
+        overlap_intervals.insert(overlap_intervals.end(), intervals.begin(),
+                                 intervals.end());
+      }
+    } else {
+      overlap_intervals =
+          _get_intervals_containing_point(nodes.at(node).x_mid, edge.id, node);
+      if (nodes.at(node).has_left_child()) {
+        auto intervals =
+            get_intervals_overlapping_with(edge, left_child_offset);
+        overlap_intervals.insert(overlap_intervals.end(), intervals.begin(),
+                                 intervals.end());
+      }
+      if (nodes.at(node).has_right_child()) {
+        auto intervals =
+            get_intervals_overlapping_with(edge, right_child_offset);
+        overlap_intervals.insert(overlap_intervals.end(), intervals.begin(),
+                                 intervals.end());
+      }
     }
-    if (is_exist(right_child_offset)) {
-      get_overlapping_intervals(edge, right_child_offset, overlap_interval);
-    }
+    return overlap_intervals;
   }
 }
 
-inline void interval_tree::add_segment(interval* edge, const int node) {
+inline void interval_tree::_add_interval_to_node(interval& edge,
+                                                 const int node) {
   auto start_insert = std::lower_bound(
       nodes.at(node).lines_start.begin(), nodes.at(node).lines_start.end(),
-      edge->x_left, [](interval a, const int b) { return a.x_left < b; });
-  nodes.at(node).lines_start.insert(start_insert, *edge);
+      edge.x_left, [](interval a, const int b) { return a.x_left < b; });
+  nodes.at(node).lines_start.insert(start_insert, edge);
   auto end_insert = std::lower_bound(
       nodes.at(node).lines_end.begin(), nodes.at(node).lines_end.end(),
-      edge->x_right, [](interval a, const int b) { return a.x_right < b; });
-  nodes.at(node).lines_end.insert(end_insert, *edge);
+      edge.x_right, [](interval a, const int b) { return a.x_right < b; });
+  nodes.at(node).lines_end.insert(end_insert, edge);
 }
 
-inline void interval_tree::add_interval(interval* edge, const int node) {
-  if (edge->x_left > nodes.at(node).x_mid) {
-    if (is_exist(nodes.at(node).right_child)) {
-      add_interval(edge, nodes.at(node).right_child);
-    } else {
-      nodes.at(node).right_child = add_node(edge);
-    }
-  } else if (edge->x_right < nodes.at(node).x_mid) {
-    if (is_exist(nodes.at(node).left_child)) {
-      add_interval(edge, nodes.at(node).left_child);
-    } else {
-      nodes.at(node).left_child = add_node(edge);
-    }
+inline void interval_tree::add_interval(interval& edge, int node) {
+  if (nodes.empty()) {
+    _add_node(edge);
   } else {
-    add_segment(edge, node);
+    if (edge.x_left > nodes.at(node).x_mid) {
+      if (nodes.at(node).has_right_child()) {
+        add_interval(edge, nodes.at(node).right_child);
+      } else {
+        nodes.at(node).right_child = _add_node(edge);
+      }
+    } else if (edge.x_right < nodes.at(node).x_mid) {
+      if (nodes.at(node).has_left_child()) {
+        add_interval(edge, nodes.at(node).left_child);
+      } else {
+        nodes.at(node).left_child = _add_node(edge);
+      }
+    } else {
+      _add_interval_to_node(edge, node);
+    }
   }
 }
 
-inline void interval_tree::delete_interval(interval* edge, const int node) {
-  if (edge->x_left > nodes.at(node).x_mid) {
+inline void interval_tree::delete_interval(interval& edge, const int node) {
+  if (edge.x_left > nodes.at(node).x_mid) {
     delete_interval(edge, nodes.at(node).right_child);
-  } else if (edge->x_right < nodes.at(node).x_mid) {
+  } else if (edge.x_right < nodes.at(node).x_mid) {
     delete_interval(edge, nodes.at(node).left_child);
   } else {
     auto start_erase = std::lower_bound(
         nodes.at(node).lines_start.begin(), nodes.at(node).lines_start.end(),
-        edge, [](interval a, interval* b) {
-          return a.x_left < b->x_left && -a.id != b->id;
+        edge, [](interval a, interval b) {
+          return a.x_left < b.x_left && -a.id != b.id;
         });
     nodes.at(node).lines_start.erase(start_erase);
     auto end_erase = std::lower_bound(
         nodes.at(node).lines_end.begin(), nodes.at(node).lines_end.end(), edge,
-        [](interval a, interval* b) {
-          return a.x_right < b->x_right && -a.id != b->id;
+        [](interval a, interval b) {
+          return a.x_right < b.x_right && -a.id != b.id;
         });
     nodes.at(node).lines_end.erase(end_erase);
   }
