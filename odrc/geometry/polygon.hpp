83ce9392c3eea47f844e39f2e0d4d62b438fbeaf
Zhuolun Leon HE <zleonhe@gmail.com>
2023-03-10 13:37:06 +0800

    refactor(geometry): add type erased polygon

--- odrc/geometry/polygon.hpp
+++ odrc/geometry/polygon.hpp
@@ -4,29 +4,64 @@
 #include <memory>
 #include <vector>
 
+#include <odrc/geometry/geometry.hpp>
 #include <odrc/geometry/point.hpp>
 
 namespace odrc::geometry {
 
-template <typename Point   = point2d<>,
-          bool IsClockWise = true,
+template <typename GeoSpace = geometry_space<>>
+class polygon {
+ public:
+  template <typename Polygon>
+  polygon(const Polygon& polygon)
+      : _this(std::make_shared<model<Polygon>>(polygon)) {}
+
+  std::size_t size() const { return _this->size(); }
+
+ private:
+  class concept {
+   public:
+    virtual ~concept()                        = default;
+    virtual std::size_t size() const noexcept = 0;
+  };
+  template <typename Polygon>
+
+  class model : public concept {
+   public:
+    model(const Polygon& polygon) : _polygon(polygon) {}
+    model(Polygon&& polygon) : _polygon(std::move(polygon)) {}
+    std::size_t size() const noexcept override { return _polygon.size(); }
+
+   private:
+    Polygon _polygon;
+  };
+
+  std::shared_ptr<concept> _this;
+};
+
+namespace detail {
+
+template <typename GeoSpace = geometry_space<>,
+          bool IsClockWise  = true,
           template <typename T, typename Allocator> typename Container =
               std::vector,
           template <typename T> typename Allocator = std::allocator>
 class polygon {
  public:
-  using point_type = Point;
-  using point_list = Container<Point, Allocator<Point>>;
+  using vertex      = point<GeoSpace>;
+  using vertex_list = Container<vertex, Allocator<vertex>>;
 
   polygon() = default;
-  polygon(const point_list& points) : _points(points) {}
+  polygon(const vertex_list& vertices) : _vertices(vertices) {}
   template <typename Iterator>
-  polygon(Iterator begin, Iterator end) : _points(begin, end) {}
-  polygon(std::initializer_list<Point> init) : _points(init) {}
+  polygon(Iterator begin, Iterator end) : _vertices(begin, end) {}
+  polygon(std::initializer_list<vertex> init) : _vertices(init) {}
 
-  constexpr std::size_t size() const noexcept { return _points.size(); }
+  constexpr std::size_t size() const noexcept { return _vertices.size(); }
 
  private:
-  point_list _points;
+  vertex_list _vertices;
 };
+
+}  // namespace detail
 }  // namespace odrc::geometry
--- tests/geometry/polygon.test.cpp
+++ tests/geometry/polygon.test.cpp
@@ -8,23 +8,26 @@
 #include <odrc/geometry/point.hpp>
 
 TEST_SUITE("[OpenDRC] odrc::geometry polygon tests") {
-  using p = odrc::geometry::point2d<>;
+  using poly_t  = odrc::geometry::detail::polygon<>;
+  using point_t = odrc::geometry::point<>;
+  using p2d     = odrc::geometry::pointnd<>;
   TEST_CASE("test empty polygon") {
-    odrc::geometry::polygon<p> poly;
+    odrc::geometry::polygon poly{poly_t{}};
     CHECK_EQ(poly.size(), 0);
   }
   TEST_CASE("test polygon with point list") {
-    std::vector<p>             points{{1, 1}, {1, 3}, {3, 3}, {3, 1}};
-    odrc::geometry::polygon<p> poly(points);
+    std::vector<point_t>    points{p2d{1, 1}, p2d{1, 3}, p2d{3, 3}, p2d{3, 1}};
+    odrc::geometry::polygon poly{poly_t(points)};
     CHECK_EQ(poly.size(), points.size());
   }
   TEST_CASE("test polygon with iterators") {
-    std::list<p>               points{{1, 1}, {1, 3}, {3, 3}, {3, 1}};
-    odrc::geometry::polygon<p> poly(points.begin(), points.end());
+    std::list<point_t>      points{p2d{1, 1}, p2d{1, 3}, p2d{3, 3}, p2d{3, 1}};
+    odrc::geometry::polygon poly(poly_t(points.begin(), points.end()));
     CHECK_EQ(poly.size(), points.size());
   }
   TEST_CASE("test polygon with initializer list") {
-    odrc::geometry::polygon<p> poly({{1, 1}, {1, 3}, {3, 3}, {3, 1}});
+    odrc::geometry::polygon poly(
+        poly_t({p2d{1, 1}, p2d{1, 3}, p2d{3, 3}, p2d{3, 1}}));
     CHECK_EQ(poly.size(), 4);
   }
 }
